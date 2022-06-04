d4217d24060e463567d85c2cec11c04a18524c26
Juan Pablo Acosta <juanpabloacosta@Juans-MacBook-Air.local>
2022-06-03 19:10:25 -0600

    Fixed bug finding task id when no tasks in index.js with optional chaining

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -46,8 +46,8 @@ export const updateUserTasks = async (req, res) => {
 
     try {
         found.tasks = updatedTasks;
-        await found.save()
-        res.status(201).json({ updateTasks: true, newId: found.tasks[found.tasks.length - 1]._id });
+        await found.save();
+        res.status(201).json({ updateTasks: true, newId: found.tasks[found.tasks.length - 1]?._id || 0 });
     } catch (err) {
         res.status(409).json({ message: err.message })
     }
